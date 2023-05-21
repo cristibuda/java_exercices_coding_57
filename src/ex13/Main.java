bcbef7825d14fabc576b2e97ed91a4bccc445ab5
cristibuda <buda.cristian@protonmail.com>
2023-05-21 12:05:24 +0300

    Am creat jocul spanzuratoarea si x si zero.

--- src/ex13/Main.java
+++ src/ex13/Main.java
@@ -1,4 +1,48 @@
 package ex13;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        String word = "abcabcabc";
+        String[] player = new String[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            if (i == 0 || i == word.length() - 1) {
+                System.out.print(word.charAt(i));
+                player[i] = word.charAt(i) + "";
+            } else {
+                player[i] = "_";
+                System.out.print("_");
+            }
+
+            System.out.print(" ");
+        }
+        Scanner scanner = new Scanner(System.in);
+        do {
+            String input = scanner.next();
+            if (word.contains(input)) {
+
+                for( int i = 0; i<word.length(); i++){
+                    char c = word.charAt(i);
+                    if(c == input.charAt(0)){
+//                        System.out.println(input.charAt(0));
+                        player[i]= input;
+                    }
+                }
+                showArray(player);
+            } else {
+                System.out.println("Mai incearca!");
+            }
+        } while (true);
+
+    }
+
+    public static void showArray(String[] data) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+
+    }
+
 }
---
+++ src/ex14/Main.java
@@ -0,0 +1,116 @@
+package ex14;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // sa se simuleze jocul x si 0.
+        char tabel[][] = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tabel[i][j] = ' ';
+            }
+        }
+
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Introduceti i: ");
+            int i = scanner.nextInt() - 1;
+            System.out.println();
+            System.out.print("Introduceti j: ");
+            int j = scanner.nextInt() - 1;
+            System.out.println();
+            System.out.print("Introduceti x sau 0: ");
+            char x0 = scanner.next().charAt(0);
+            tabel[i][j] = x0;
+            printTable(tabel);
+
+            boolean resultLinie = checkLinie(tabel);
+            if (resultLinie == true) {
+                break;
+            }
+            boolean resultColoana = checkColoana(tabel);
+            if (resultColoana) {
+                break;
+            }
+            boolean resultDiagonalaPrincipala = checkDiagonalaPrincipala(tabel);
+            if (resultDiagonalaPrincipala) {
+                break;
+            }
+            boolean resultDiagonalaSecundara = checkDiagonalaSecundara(tabel);
+            if (resultDiagonalaSecundara) {
+                break;
+            }
+        }
+
+    }
+
+    public static boolean checkLinie(char tabel[][]) {
+        for (int i = 0; i < 3; i++) {
+            String line = "";
+            for (int j = 0; j < 3; j++) {
+                line = line + tabel[i][j];
+            }
+            if (line.equals("xxx") || line.equals("000")) {
+                System.out.println("Avem un castigator!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkColoana(char tabel[][]) {
+        for (int i = 0; i < 3; i++) {
+            String coloana = "";
+            for (int j = 0; j < 3; j++) {
+                coloana = coloana + tabel[j][i];
+            }
+
+            if (coloana.equals("xxx") || coloana.equals("000")) {
+                System.out.println("Avem un castigator!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkDiagonalaPrincipala(char tabel[][]) {
+        String diagonalaPrincipala = "";
+        for (int i = 0; i < 3; i++) {
+            diagonalaPrincipala = diagonalaPrincipala + tabel[i][i];
+        }
+        if (diagonalaPrincipala.equals("xxx") || diagonalaPrincipala.equals("000")) {
+            System.out.println("Avem un castigator!");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean checkDiagonalaSecundara(char tabel[][]) {
+        String diagonalaSecundara = "";
+        for (int i = 0; i < 3; i++) {
+            int j = 2 - i;
+            diagonalaSecundara = diagonalaSecundara + tabel[i][j];
+        }
+        if (diagonalaSecundara.equals("xxx") || diagonalaSecundara.equals("000")) {
+            System.out.println("Avem un castigator!");
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void printTable(char tabel[][]) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(tabel[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-----------");
+        }
+    }
+
+}
